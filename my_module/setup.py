c3970eeb413bc30d310f8f1218eee686ace475b1
snailuncle <1789500304@qq.com>
2018-03-28 12:31:48 +0800

    第一次发布自定义模块

---
+++ my_module/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='my_module',
+    version='1.0',
+    py_modules=['format_headers'],
+    author='qq1789500304',
+    author_email='1789500304@qq.com',
+    url='http://www.xiaoxinfeng.com.cn',
+    descriptioin='给谷歌浏览器复制的headers添加引号,让爬虫可以直接使用',
+)
---
+++ my_module/模块发布步骤.py
@@ -0,0 +1,19 @@
+# 1       Linux命令（当前位置位于模块文件夹）：
+#          python setup.py build
+
+# 2       Linux命令（当前位置位于模块文件夹）：
+#         python setup.py sdist
+#         打包后,生成最终发布压缩包 moduleName-version.tar.gz ,
+
+
+# 3       模块安装
+
+#        找到模块的压缩包
+#        解压
+#        进入文件夹
+#        执行命令sudo python setup.py install
+
+
+# 4       ps：如果在install的时候，执行目录安装，可以使用python setup.py install --prefix=安装路径
+
+
