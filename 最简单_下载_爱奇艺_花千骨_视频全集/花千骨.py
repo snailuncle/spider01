45e4f324c24b8513862cdc81d4d6f072eb1acc5f
snailuncle <1789500304@qq.com>
2018-04-06 15:32:07 +0800

    花千骨视频下载,捕获异常

--- 最简单_下载_爱奇艺_花千骨_视频全集/花千骨.py
+++ 最简单_下载_爱奇艺_花千骨_视频全集/花千骨.py
@@ -54,28 +54,30 @@ def huaqiangu_urls():
 def video_download(url):
     # url='http://www.iqiyi.com/v_19rroheauw.html'
     # url='http://www.iqiyi.com/v_19rrnsr5zw.html'
-
-    #提取视频名字
-    cmd='you-get -i "%s"  '%url
-    #执行命令
-    p = sp.Popen(cmd, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE, shell=True) 
-    #获得返回结果并解码
-    out = p.stdout.read().decode("utf-8")
-    name=re.findall(r"(?<=title:).*",out)[0].strip()
-    print(name)
-    # 使用--output-dir/-o 设定路径, --output-filename/-O 设定输出文件名:
-    # $ you-get -o ~/Videos -O zoo.webm 'https://www.youtube.com/watch?v=jNQXAC9IVRw'
-    cmd='you-get --format=LD -o D:/spider01/最简单_下载_爱奇艺_花千骨_视频全集 -O %s  "%s"  '%(name,url)
-    # you-get --format=LD [URL]
-    #执行命令
-    p = sp.Popen(cmd, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT, shell=True) 
-    #获得返回结果并解码
-    # out = p.stdout.read().decode("utf-8")
-    line = p.stdout.readline()
-    while line:
-        line=line.decode("utf-8")
-        print(line)
+    try:
+        #提取视频名字
+        cmd='you-get -i "%s"  '%url
+        #执行命令
+        p = sp.Popen(cmd, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.PIPE, shell=True) 
+        #获得返回结果并解码
+        out = p.stdout.read().decode("utf-8")
+        name=re.findall(r"(?<=title:).*",out)[0].strip()
+        print(name)
+        # 使用--output-dir/-o 设定路径, --output-filename/-O 设定输出文件名:
+        # $ you-get -o ~/Videos -O zoo.webm 'https://www.youtube.com/watch?v=jNQXAC9IVRw'
+        cmd='you-get --format=LD -o D:/spider01/最简单_下载_爱奇艺_花千骨_视频全集 -O %s  "%s"  '%(name,url)
+        # you-get --format=LD [URL]
+        #执行命令
+        p = sp.Popen(cmd, stdin=sp.PIPE, stdout=sp.PIPE, stderr=sp.STDOUT, shell=True) 
+        #获得返回结果并解码
+        # out = p.stdout.read().decode("utf-8")
         line = p.stdout.readline()
+        while line:
+            line=line.decode("utf-8")
+            print(line)
+            line = p.stdout.readline()
+    except Exception as e:
+        print(e)
 
 # import subprocess
 # subp=subprocess.Popen('python -u /tmp/test.py',shell=True,stdout=subprocess.PIPE)
